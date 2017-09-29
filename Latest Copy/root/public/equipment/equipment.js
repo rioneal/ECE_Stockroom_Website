0dd9aad64dc64ca18f37671f82ad99088c76119a
rioneal <roneal96@gmail.com>
2017-09-29 12:15:41 -0500

    Repository setup
    9/29/2017 import of new project

---
+++ Latest Copy/root/public/equipment/equipment.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var equipmentRouter = express.Router();
+
+
+var bodyParser = require('body-parser');
+var multer = require('multer'); 
+var multiform = multer();
+
+console.log(__dirname);
+
+
+var mongoose = require('mongoose');
+
+/*mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Stockroom');
+*/
+
+var equipmentSchema = mongoose.Schema({
+	_id: String,
+	owner: String, 
+	description: String
+});
+
+
+var equipment = mongoose.model("equipment", equipmentSchema);
+
+equipmentRouter.use(bodyParser.json());
+equipmentRouter.use(bodyParser.urlencoded({extended: true}));
+equipmentRouter.use(multiform.array());
+
+equipmentRouter.get('/addequipment', function(req, res){
+	res.sendFile(__dirname + '/equipment.html');
+});
+
+equipmentRouter.post('/addequipment', function(req,res){
+	var equipmentInfo = req.body; 
+	
+	
+	var newEquipment = new equipment({
+		_id: equipmentInfo.equip_entry, 
+		description: equipmentInfo.equip_description, 
+		owner: null
+	});
+	
+	
+	newEquipment.save(function(err, res){
+		if(err)
+		{
+			console.log(err); 
+		}
+		else
+		{
+			console.log("equipment added successfully");
+		}
+	});
+	
+});
+
+module.exports = equipmentRouter; 
